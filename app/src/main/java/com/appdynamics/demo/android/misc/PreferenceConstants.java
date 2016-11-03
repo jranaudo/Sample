aff776e6ca2ef5d427020afaaabda6533c147ae6
jranaudo <jranaudo@gmail.com>
2016-11-03 13:41:39 -0400

    Initial Commit

---
+++ app/src/main/java/com/appdynamics/demo/android/misc/PreferenceConstants.java
@@ -0,0 +1,7 @@
+package com.appdynamics.demo.android.misc;
+
+public final class PreferenceConstants {
+    public final static String END_POINT_URL = "http://104.197.181.44/appdynamicspilot";
+    public static final String EUM_APP_KEY = "";
+    public static final String EUM_COLLECTOR_URL = "";
+}
